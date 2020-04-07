b0d130180261b7626f8d74dcdadb9ed54b3bce05
FishChipos <63272281+FishChipos@users.noreply.github.com>
2020-04-07 10:54:14 +0700

    Update onButtonClick.js

--- onButtonClick.js
+++ onButtonClick.js
@@ -1,4 +1,22 @@
+timesClicked = 0
+
 function onButtonClick () {
+	
+	if (timesClicked == 1) {
+		document.getElementById("newText").innerHTML = "Isn't a thank you enough?";
+		timesClicked++;
+		
+		return;
+	}
+	
+	if (timesClicked == 2) {
+		document.getElementById("newText").innerHTML = "Fine I'll just disable the button.";
+		
+		document.getElementById("btn").removeEventListener("click", onButtonClick);
+		document.getElementById("btn").setAttribute("class", "disabled-button");
+	
+		return;
+	}
 	
 	let wrapper = document.getElementById("wrapper");
 	let newParagraph = document.createElement("p");
@@ -9,6 +27,5 @@ function onButtonClick () {
 	newParagraph.appendChild(newText);
 	wrapper.appendChild(newParagraph);
 	
-	document.getElementById("btn").removeEventListener("click", onButtonClick);
-	document.getElementById("btn").setAttribute("class", "disabled-button");
+	timesClicked++;
 }
